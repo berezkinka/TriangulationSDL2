a0f8d0190351842770f4b27267fe4308d0b64768
k.berezkin <berezkinka@gmail.com>
2025-04-06 15:14:02 +0300

    Добавлены основные структуры данных.

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,3 +1,22 @@
-int WinMain() {
-  return 0;
+#include <vector>
+
+class Point {
+public:
+  double x, y;
+
+  Point(double x = 0, double y = 0) : x(x), y(y) {}
+};
+
+class Triangle {
+  public:
+      Point a, b, c;
+  
+      Triangle(const Point& a, const Point& b, const Point& c) : a(a), b(b), c(c) {}
+};
+
+std::vector<Point> points;
+std::vector<Triangle> triangles;
+
+int WinMain(int argc, char** args) {
+	return 0;
 }
