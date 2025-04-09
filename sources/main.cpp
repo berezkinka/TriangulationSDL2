db247f7bd5b16cfdc005cc97eba4f2b5597eadc9
k.berezkin <berezkinka@gmail.com>
2025-04-09 23:08:38 +0300

    Продолжение работы над алгоритмом триангуляции. Добавлена функция для проверки попадания точки в описанную окружность треугольника. Реализована перегрузка операторов для сравнения точек, рёбер и треугольников. Добавлены прочие вспомогательные функции.

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,26 +7,33 @@ public:
   double x, y;
 
   Point(double x = 0, double y = 0) : x(x), y(y) {}
+  bool operator==(const Point& other) const {
+    return x == other.x && y == other.y;
+  }
 };
 
-class Line {
-  public:
-      Point p1, p2;
+class Edge {
+public:
+  Point p1, p2;
   
-      Line(const Point& p1, const Point& p2) : p1(p1), p2(p2) {}
-  };
+  Edge(const Point& p1, const Point& p2) : p1(p1), p2(p2) {}
+  bool operator==(const Edge& other) const {
+    return (p1 == other.p1 && p2 == other.p2) || (p1 == other.p2 && p2 == other.p1);
+  }
+};
 
 class Triangle {
-  public:
-      Point a, b, c;
-      Line ab, bc, ca;
+public:
+  Point a, b, c;
+  Edge ab, bc, ca;
   
-      Triangle(const Point& a, const Point& b, const Point& c) 
-        : a(a), b(b), c(c), 
-          ab(a, b),
-          bc(b, c),
-          ca(c, a)
-      {}
+  Triangle(const Point& a, const Point& b, const Point& c) : a(a), b(b), c(c), ab(a, b), bc(b, c), ca(c, a) {}
+  std::vector<Edge> getEdges() const {
+    return {ab, bc, ca};
+  }
+  bool hasEdge(const Edge& other) {
+    return (ab == other) || (bc == other) || (ca == other);
+  }
 };
 
 std::vector<Point> points;
@@ -38,6 +45,7 @@ SDL_Renderer *renderer;
 bool init();
 void kill();
 bool loop();
+bool isPointInCircumcircle(const Point& p, const Triangle& tri);
 void triangulation();
 
 bool init() {
@@ -114,21 +122,53 @@ void kill() {
   SDL_Quit();
 }
 
+// Функция проверяет, что точка попадает в описанную окружность треугольника.
+bool isPointInCircumcircle(const Point& p, const Triangle& tri) {
+  double ax = tri.a.x - p.x, ay = tri.a.y - p.y;
+  double bx = tri.b.x - p.x, by = tri.b.y - p.y;
+  double cx = tri.c.x - p.x, cy = tri.c.y - p.y;
+
+  double det = (
+      (ax * ax + ay * ay) * (bx * cy - by * cx) -
+      (bx * bx + by * by) * (ax * cy - ay * cx) +
+      (cx * cx + cy * cy) * (ax * by - ay * bx)
+  );
+
+  return det > 0;
+}
+
 void triangulation() {
   // Создаем супер-треугольник (он должен охватывать все точки).
   double minX = points[0].x, maxX = points[0].x;
   double minY = points[0].y, maxY = points[0].y;
+  
   for (const auto& p : points) {
       minX = std::min(minX, p.x);
       maxX = std::max(maxX, p.x);
       minY = std::min(minY, p.y);
       maxY = std::max(maxY, p.y);
   }
+
   double dtx = maxX - minX, dty = maxY - minY;
   Point a(minX - dtx, minY - dty);
   Point b(maxX + dtx, minY - dty);
   Point c(minX + dtx / 2, maxY + dty);
+
   triangles.push_back(Triangle(a, b, c));
+
+  // Добавляем точки.
+  for (const auto& point : points) {
+    
+    std::vector<Triangle> badTriangles; // Треугольники, в описанную окружность которых попадает точка.
+    
+    for (const auto& triangle : triangles) {
+      if (isPointInCircumcircle(point, triangle)) {
+          badTriangles.push_back(triangle);
+      }
+    }
+    // Работа с рёбрами. 
+
+  }
 }
 
 int WinMain(int argc, char** args) {
